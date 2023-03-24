38b21dde234e46066f8ef8e28c2c32f616158dcd
Matt DeMichele <mattdemichele@gmail.com>
2023-03-24 17:52:05 -0400

    add sane default minWidth

--- src/features/ComponentsDisplay/components/ComponentWrapper.tsx
+++ src/features/ComponentsDisplay/components/ComponentWrapper.tsx
@@ -3,6 +3,7 @@ import { Box, styled } from "@mui/material";
 // NOTE: maybe we want to do more here about the sizing of a component container?
 // mobile view? Portrait vs landscape? etc...
 export const ComponentWrapper = styled(Box)(() => ({
+  minWidth: 200,
   width: '100%',
   minHeight: '10vh',
   border: '1px solid black',
